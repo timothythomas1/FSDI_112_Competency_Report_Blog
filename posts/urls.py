7eb2de10e8585d5329c467dd27aa9e9e38ab24d9
timothythomas1 <tuxes-parlay0s@icloud.com>
2022-09-30 19:03:10 -0700

    Fixed Traceback Error. Was a url issue

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView
-)
+from .views import PostListView, PostDetailView, PostCreateView
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='list'),
-    path('<path:path>', PostDetailView.as_view(), name='new'),
-    path('new', PostCreateView.as_view(), name='new'),
+    path('<int:pk>', PostDetailView.as_view(), name='detail'),
+    path('new/', PostCreateView.as_view(), name='new'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView, CreateView
+# from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
